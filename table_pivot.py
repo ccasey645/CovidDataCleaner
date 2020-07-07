0506605acc0574563bf2cb064c45d4a337151dd5
ccasey645 <ccasey645@hotmail.com>
2020-07-07 15:55:00 -0400

    Add AWS lambda function version of function.

--- table_pivot.py
+++ table_pivot.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import boto3
 import sys
 import traceback
+from io import StringIO
+import json
 
 def process_file(file_name):
     data = pd.read_csv(file_name)
@@ -14,6 +17,30 @@ def process_file(file_name):
                               value_name="Running Sum")
     old_file_name, old_file_extension = file_name.split(".")
     pivoted_table.to_csv("{old_file_name}_pivoted.csv".format(old_file_name=old_file_name))
+
+def lambda_handler(event, context):
+    file_name = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv"
+    data = pd.read_csv(file_name)
+    bucket_name = "usa-covid-by-county-time-series"
+    csv_buffer = StringIO()
+
+    columns_to_pivot = []
+    for col in data.columns:
+        if col != "countyFIPS" or col != "County Name" or col != "stateFIPS" or col != "State":
+            columns_to_pivot.append(col)
+
+    pivoted_table = data.melt(id_vars=["countyFIPS", "County Name", "State", "stateFIPS"], var_name="Date",
+                              value_name="Running Sum")
+    old_file_name, old_file_extension = file_name.split(".")
+    pivoted_table.to_csv(csv_buffer)
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object(bucket_name, "covid_confirmed_usafacts_pivoted.csv").put(Body=csv_buffer.getvalue())
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'success':True})
+    }
+
 
 if __name__ == "__main__":
     try:
